8e339d53b3d0fd6f9ddd2352e1b5e4c3e94098e0
tw-rw <tarahmr@gmail.com>
2024-11-09 17:06:23 +0330

    search engine

--- src/java.js
+++ src/java.js
@@ -1,1 +1,9 @@
-alert("hello world!");
+function showcity(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector(".search-box");
+  let city = document.querySelector("h2");
+  city.innerHTML = cityElement.value;
+}
+
+let searchForm = document.querySelector(".search-form");
+searchForm.addEventListener("submit", showcity);
